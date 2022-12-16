2a481f8763489ddd4028ded91b42fe91ebd87297
Madlena Ivanova <madlena.iv@abv.bg>
2022-12-16 11:42:02 +0200

    added tables Car and Category + first migrartion

---
+++ CarRentingSystem/Data/DataConstants.cs
@@ -0,0 +1,14 @@
+﻿namespace CarRentingSystem.Data
+{
+    public class DataConstants
+    {
+        //for class Car
+        public const int CarBrandMaxLength = 20;
+        public const int CarModelMaxLength = 30;
+        public const int CarDescriptionMaxLength = 10000;
+        //public const int CarYearMinLength = 2000;
+        //public const int CarYearMaxLength = 2100;
+
+
+    }
+}
